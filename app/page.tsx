c59273a9f2229798a21a5495d8c2d08c711795aa
FabinhoEdinei <59501299+FabinhoEdinei@users.noreply.github.com>
2026-01-28 10:15:06 -0300

    Atualizar o page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation"
 import { createClient } from "@/lib/supabase/server"
+import Link from "next/link"
 
 export default async function Home() {
   const supabase = await createClient()
